d0b7354943fa37962d7c0708ea1f74778f60b268
dramalife_vb_u16d <dramalife@live.com>
2020-10-12 13:34:24 +0800

    [protocol][mqtt] A&U sample mqtt - OneNET.

--- protocol/paho.mqtt_dl_sample/dl_mqtt_client_pub.c
+++ protocol/paho.mqtt_dl_sample/dl_mqtt_client_pub.c
@@ -19,17 +19,8 @@
 #include <string.h>
 #include "MQTTClient.h"
 
-//#define ADDRESS     "tcp://mqtt.eclipse.org:1883"
-#define ADDRESS     "tcp://183.230.40.39:6002"
-//#define CLIENTID    "ExampleClientPub"
-#define CLIENTID    "636888257"
-#define USERNAME	"376241"
-#define PASSWORD	"iot00001"
+#include "config_mqtt_dl.h"
 
-#define TOPIC       "MQTT Examples"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
 
 int main(int argc, char* argv[])
 {
--- protocol/paho.mqtt_dl_sample/install_exec.sh
+++ protocol/paho.mqtt_dl_sample/install_exec.sh
@@ -19,7 +19,12 @@ fi
 
 
 # Install files
+## src
 ln -svf ${CURRDIR}/dl_mqtt_client_pub.c	${PAHO_MQTT_DIR}/src/samples/dl_mqtt_client_pub.c
+ln -svf ${CURRDIR}/dl_mqtt_client_sub.c	${PAHO_MQTT_DIR}/src/samples/dl_mqtt_client_sub.c
+## header
+ln -svf ${CURRDIR}/config_mqtt_dl.h	${PAHO_MQTT_DIR}/src/config_mqtt_dl.h
+## scripts
 cp -vf ${CURRDIR}/Makefile_dl		${PAHO_MQTT_DIR}/Makefile
 cp -vf ${CURRDIR}/CMakeLists.txt_dl	${PAHO_MQTT_DIR}/src/samples/CMakeLists.txt
 echo -e ${DLTERM_LGREEN} "\nDONE :"${DLTERM_NONE}" Install files.\n"
@@ -31,5 +36,19 @@ echo -e ${DLTERM_LGREEN}"\nDONE :"${DLTERM_NONE}" Compile.\n"
 
 
 # Exec
-${PAHO_MQTT_DIR}build/output/samples/dl_mqtt_client_pub
-echo -e ${DLTERM_LGREEN}"\nDONE :"${DLTERM_NONE}" Exec.\n"
+if [ "$1" = "exec" ]
+then
+	echo ""
+	if [ "$2" = "pub" ]
+	then
+		${PAHO_MQTT_DIR}build/output/samples/dl_mqtt_client_pub
+		echo -e ${DLTERM_LGREEN}"\nDONE :"${DLTERM_NONE}" Exec (${2}).\n"
+	elif [ "$2" = "sub" ]
+		${PAHO_MQTT_DIR}build/output/samples/dl_mqtt_client_pub
+		echo -e ${DLTERM_LGREEN}"\nDONE :"${DLTERM_NONE}" Exec (${2}).\n"
+	else
+		echo -e ${DLTERM_LGREEN}"\nDONE :"${DLTERM_NONE}" Not Exec (${2})!\n"
+	fi
+else
+	echo -e ${DLTERM_LGREEN}"\nDONE :"${DLTERM_NONE}" No need Exec!\n"
+fi
