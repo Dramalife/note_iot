92540dcc529a9771c5b837c5f39db8b2f4f12f9b
Dramalife_rk3328 <dramalife@live.com>
2021-01-20 14:32:27 +0800

    [rt-thread][app] Add symlink for tag; Add template generation script.

---
+++ app/rtt/embed/template.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "\
+/*
+ * [note_iot] \"${1}\" related file
+ * Copyright (C) 2019 Dramalife <dramalife@live.com>
+ * 
+ * This file is part of [note_iot](https://github.com/Dramalife/note_iot.git)
+ * 
+ * [note_iot] is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Init : $(date)
+ *  
+ * Update
+ *
+ */\
+ "
